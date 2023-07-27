0f1623d8a693ffa47aea36718f9a3328cf3621c0
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-07-27 09:09:14 +0200

    Add Create and Read parts of functionality

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,15 @@
+"Libraries"
 from django.db import models
 
+# pylint:disable = E0307
+
 # Create your models here.
+
+
+class Item(models.Model):
+    "Set up db model for Item"
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
