9689fd5881a271c2826586f9e4d170666ce8bb22
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-07-27 16:47:43 +0200

    Edit settings.py

--- dango_todo/settings.py
+++ dango_todo/settings.py
@@ -15,8 +15,7 @@ import os
 import dj_database_url
 if os.path.isfile("env.py"):
     import env
-
-development = env.DEVELOPMENT
+    development = env.DEVELOPMENT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
